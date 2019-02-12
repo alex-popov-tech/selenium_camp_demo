709abd0671295a47e7286c271ac32dc11c7f7917
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-12 19:37:58 +0200

    Update results.ts

--- webdriverio_demo/pages/results.ts
+++ webdriverio_demo/pages/results.ts
@@ -1,15 +1,11 @@
-// import { be, Browser, have } from 'selenidejs';
 import { Navigation } from '../widgets/navigation';
 
 export class Results {
-
-    // private readonly navigationMenu = new Navigation();
 
     async shouldHaveNthResults(quantity) {
         await browser.waitUntil(async () => {
             return quantity === await browser.$$('.g .r').length;
         }, 5000);
-        // await Browser.all('.g .r').should(have.size(quantity));
     }
 
     async followNthResultLink(index) {
@@ -25,13 +21,6 @@ export class Results {
             return index === filtered.length + 1;
         }, 5000);
         await browser.$$('.g .r')[index].$('a').click();
-        // await Browser.all('.g .r').filterBy(be.visible)
-        //     .get(index)
-        //     .element('a')
-        //     .click();
     }
 
-    // async navigateToTranslation() {
-    //     await this.navigationMenu.translate();
-    // }
 }
