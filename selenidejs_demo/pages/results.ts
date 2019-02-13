ca16c0c6a80f8f84cd566ebf89ec0fbb6e8fdecc
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-13 17:32:54 +0200

    Update results.ts

--- selenidejs_demo/pages/results.ts
+++ selenidejs_demo/pages/results.ts
@@ -2,13 +2,15 @@ import { be, Browser, have } from 'selenidejs';
 import { Navigation } from '../widgets/navigation';
 
 export class Results {
+    
+    private readonly results = Browser.all('.g .r').filterBy(be.visible);
 
     async shouldHaveNthResults(quantity) {
-        await Browser.all('.g .r').should(have.size(quantity));
+        await this.results.should(have.size(quantity));
     }
 
     async followNthResultLink(index) {
-        await Browser.all('.g .r').filterBy(be.visible)
+        await this.results
             .get(index)
             .element('a')
             .click();
