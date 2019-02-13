6ebebabe7e6d25ea592cfb9e6fd2aae1c270a845
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-13 16:29:21 +0200

    Update results.ts

--- protractor_demo/pages/results.ts
+++ protractor_demo/pages/results.ts
@@ -2,12 +2,14 @@ import { Navigation } from '../widgets/navigation';
 import {$$, browser, by, ElementArrayFinder, ElementFinder, ExpectedConditions} from 'protractor';
 
 export class Results {
-    private readonly navigation: Navigation;
-    private readonly elements: ElementArrayFinder;
+    private readonly navigation = new Navigation();
+    private readonly elements = $$('.g .r');
 
-    constructor() {
-        this.navigation = new Navigation();
-        this.elements = $$('.g .r');
+    public async followNthResultLink(index: number): Promise<void> {
+        const result = this.elements.get(index).$('a');
+        
+        await browser.wait(ExpectedConditions.elementToBeClickable(result), 5000);
+        await result.click();
     }
 
     public async shouldHaveNthResults(quantity: number): Promise<void> {
@@ -15,13 +17,6 @@ export class Results {
             let result;
             return quantity === await this.elements.count();
         }, 5000);
-    }
-
-    public async followNthResultLink(index: number): Promise<void> {
-        const container = this.elements.get(index);
-        const result = container.$('a');
-        await browser.wait(ExpectedConditions.elementToBeClickable(result), 5000);
-        await result.click();
     }
 
     public async navigateToTranslation(): Promise<void> {
