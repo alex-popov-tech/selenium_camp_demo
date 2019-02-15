7768a7d7058e63cc54eb100c29a6379a7c2dcc32
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-15 19:02:41 +0200

    Update search.ts

--- webdriverio_demo/pages/search.ts
+++ webdriverio_demo/pages/search.ts
@@ -1,13 +1,9 @@
-// import { Browser, perform } from 'selenidejs';
 import { Navigation } from '../widgets/navigation';
 
 export class Search {
 
-    // private readonly navigationMenu = new Navigation();
-
     async open() {
         await browser.url('http://google.com/ncr');
-        // await Browser.open('http://google.com/ncr');
     }
 
     async search(text) {
@@ -16,7 +12,4 @@ export class Search {
         await browser.keys("\uE007");
     }
 
-    // async navigateToTranslation() {
-    //     await this.navigationMenu.translate();
-    // }
 }
